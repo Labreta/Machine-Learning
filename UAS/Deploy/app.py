7b6ba4eae84dc36496500bba1ef1437572a2e1c1
Labreta <133194648+Labreta@users.noreply.github.com>
2023-05-21 16:20:20 +0700

    Add files via upload

---
+++ UAS/Deploy/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask import request
+import numpy as np
+import pandas as pd
+import pickle
+import joblib
+
+app = Flask(__name__)
+model = joblib.load("RFC_117(1.0.2).pkl")
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    feature = [float_features]
+    prediction = model.predict(feature)
+    return render_template("index.html", prediction_text = "{}".format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug=True)
